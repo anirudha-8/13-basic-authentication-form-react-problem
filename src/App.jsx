776e8ec45ee89182594e42a1bf9cb58f6f614d83
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-23 19:54:30 +0530

    design form

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    
-  )
+	const [userName, setUserName] = useState("");
+
+	const [password, setPassword] = useState("");
+
+	const handleForm = () => {};
+
+	return (
+		<div>
+			<form action="#">
+				<input
+					type="text"
+					name="username"
+					id="username"
+					value={userName}
+					placeholder="enter username"
+					onChange={(e) => setUserName(e.target.value)}
+				/>
+				<br />
+				<br />
+				<input
+					type="password"
+					name="password"
+					id="password"
+					value={password}
+					placeholder="enter password"
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<br />
+				<br />
+				<button onClick={handleForm}>Register / Login</button>
+			</form>
+			<br />
+			<hr />
+			<br />
+		</div>
+	);
 }
 
-export default App
+export default App;
