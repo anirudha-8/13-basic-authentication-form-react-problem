2fbd6d7fc9f7ca3e4c347a1573a671a8ce47a92b
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-24 22:50:28 +0530

    reset code structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,9 @@
 import "./App.css";
 
 function App() {
-	const handleForm = () => {};
-
 	return (
-		<div>
-			<h1>{isRegistered ? "Login" : "Register"}</h1>
-			<form action="#">
-				<input
-					type="text"
-					name="username"
-					id="username"
-					value={userName}
-					placeholder="enter username"
-					onChange={(e) => setUserName(e.target.value)}
-				/>
-				<br />
-				<br />
-				<input
-					type="password"
-					name="password"
-					id="password"
-					value={password}
-					placeholder="enter password"
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<br />
-				<br />
-				<button onClick={handleForm}>
-					{isRegistered ? "Login" : "Register"}
-				</button>
-			</form>
-			<br />
-			<hr />
-			<br />
-			<h2>
-				{isRegistered
-					? `Already have an account, please ${(
-							<button onClick={setIsRegistered(!isRegistered)}>Login</button>
-					  )}`
-					: "Please Register"}
-			</h2>
-		</div>
-	);
+		
+	)
 }
 
 export default App;
