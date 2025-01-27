7b8a63bc34c66e2c451973cc3303fc351464eecb
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-27 20:34:45 +0530

    create form for login / register

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import "./App.css";
 
 function App() {
-	return;
+	return (
+		<div>
+			<form action="#">
+				<input type="email" name="email" id="email" />
+				<input type="password" name="password" id="password" />
+				<button>Login / Register</button>
+			</form>
+		</div>
+	);
 }
 
 export default App;
