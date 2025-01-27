8450abf12e4b7de66119daf7211a6f54333fb2ed
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-27 22:36:32 +0530

    improve code quality

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,24 @@ function App() {
 
 	return (
 		<div>
-			<form action="#">
+			<form
+				action="#"
+				onSubmit={(e) => {
+					e.preventDefault();
+					if (!email && !password) {
+						alert("Both Email and Password are required!");
+						return;
+					}
+				}}
+			>
 				<input
 					type="email"
 					name="email"
 					id="email"
+					placeholder="Enter your email..."
 					value={email}
 					onChange={(e) => setEmail(e.target.value)}
+					aria-label="Email"
 				/>
 				<br />
 				<br />
@@ -24,8 +35,10 @@ function App() {
 					type="password"
 					name="password"
 					id="password"
+					placeholder="Enter your password..."
 					value={password}
 					onChange={(e) => setPassword(e.target.value)}
+					aria-label="Password"
 				/>
 				<br />
 				<br />
