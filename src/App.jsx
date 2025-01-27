ebc1b29805f13f78b8fae6669eb436081308731f
Anirudha Bele <beleanirudha8@gmail.com>
2025-01-27 22:21:22 +0530

    make uses of use states

--- src/App.jsx
+++ src/App.jsx
@@ -11,14 +11,34 @@ function App() {
 	return (
 		<div>
 			<form action="#">
-				<input type="email" name="email" id="email" />
+				<input
+					type="email"
+					name="email"
+					id="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
 				<br />
 				<br />
-				<input type="password" name="password" id="password" />
+				<input
+					type="password"
+					name="password"
+					id="password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
 				<br />
 				<br />
-				<button> Login / Register</button>
+				<button>{isRegister ? "Register" : "Login"}</button>
 			</form>
+			<p>
+				{isRegister
+					? "Already have an account? Log in."
+					: "Don't have an account? Register."}
+			</p>
+			<button onClick={() => setIsRegister(!isRegister)}>
+				{isRegister ? "Login" : "Register"}
+			</button>
 		</div>
 	);
 }
